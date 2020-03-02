738910b09b09b395f4fc2069004b395ed475b468
sodreamon <60939443+sodreamon@users.noreply.github.com>
2020-03-03 06:05:22 +0900

    .

---
+++ searchengine/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render_to_response
+from django.http import Http404, HttpResponse,
+HttpResponseRedirect
+
+def search(request):
+
+    if request.POST:
+        print request.POST['term']
+        return HttpResponseRedirect("/")
+    else:
+        return render_to_response('search.html')
